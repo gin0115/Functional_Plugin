098580103d902bcfad3bef6f077badae70cda366
Glynn <glynn.quelch@gmail.com>
2021-03-14 21:59:29 +0000

    Base in place

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Functional Plugin
+ * Author: Glynn Quelch
+ * Version: 1.0.0
+ */
+
+use function Gin0115\Functional_Plugin\{main as run};
+
+require_once __DIR__ . '/vendor/autoload.php';
+run();
---
+++ src/main.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+ * Entry point for the application.
+ * 
+ * @author Glynn Quelch <glynn@pinkcrab.co.uk>
+ */
+
+ namespace Gin0115\Functional_Plugin;
+
+function main(){
+    
+}
